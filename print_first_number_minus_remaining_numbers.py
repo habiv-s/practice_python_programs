3b41d2c4c164847286741d060bf5d9e4c12fda0d
Kryzle <kryzlesayat@gmail.com>
2026-03-08 22:17:51 +0800

    added calculation and prints the result

--- print_first_number_minus_remaining_numbers.py
+++ print_first_number_minus_remaining_numbers.py
@@ -1,3 +1,5 @@
 first_number = float(input("Enter a number: "))
 for i in range(9):
     numbers_to_minus = float(input("Enter a number: "))
+    first_number -= numbers_to_minus
+print(f"The result of the first number minus all of the remaining numbers is", first_number)
