0413d3a3b4a9f6b1a91a81a2cae4a9c0ac19c346
Kryzle <kryzlesayat@gmail.com>
2026-03-08 15:50:25 +0800

    added condition to see if first number is greater than second number

--- compare_two_numbers_print_bigger_number.py
+++ compare_two_numbers_print_bigger_number.py
@@ -1,3 +1,5 @@
-#Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
 first_number = float(input("Enter the first number: "))
 second_number = float(input("Enter the second number: "))
+
+if first_number > second_number:
+    print(f"The bigger number is {first_number}.")
