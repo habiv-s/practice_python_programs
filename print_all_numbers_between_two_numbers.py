94f286845577a23ffafb035dd8ed07fbf85aa5ba
Kryzle <kryzlesayat@gmail.com>
2026-03-08 23:01:56 +0800

    added if-elif-else to print numbers between inputs and handle equal numbers

--- print_all_numbers_between_two_numbers.py
+++ print_all_numbers_between_two_numbers.py
@@ -1,5 +1,11 @@
 first_number = int(input("Enter the first number: "))
 second_number = int(input("Enter the second number: "))
 
-for i in range(first_number+1, second_number):
-    print(i)
+if first_number < second_number:
+    for i in range(first_number+1, second_number):
+        print(i)
+elif second_number < first_number:
+    for i in range(second_number+1, first_number):
+        print(i)
+else:
+    print("The numbers you entered are just the same, so it does not have numbers in between.")
