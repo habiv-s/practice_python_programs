1197e33a39df17e8f4bf1403de78d657aafb3336
Kryzle <kryzlesayat@gmail.com>
2026-03-08 18:17:34 +0800

    added equation and prints quotient without decimal point (floor division)

--- print_two_numbers_quotient_without_decimal_point.py
+++ print_two_numbers_quotient_without_decimal_point.py
@@ -1,2 +1,3 @@
 first_number = int(input("Enter the first number: "))
 second_number = int(input("Enter the second number: "))
+print(f"The quotient of two numbers is ", first_number // second_number)
