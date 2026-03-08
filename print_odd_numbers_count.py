1a074c2fa41c9e1d931c3025bbaad591eb1d06e4
Kryzle <kryzlesayat@gmail.com>
2026-03-08 19:47:10 +0800

    added modulus condition to detect odd numbers and print count

--- print_odd_numbers_count.py
+++ print_odd_numbers_count.py
@@ -1,2 +1,6 @@
+check_odd_numbers = 0
 for i in range(10):
     number = int(input("Enter number: "))
+    if number % 2 != 0:
+        check_odd_numbers += 1
+print(f"ODD NUMBERS COUNT: ", check_odd_numbers)
