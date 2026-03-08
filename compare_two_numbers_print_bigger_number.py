e79d70156666cf4860b6221b02f45630c36606f6
Kryzle <kryzlesayat@gmail.com>
2026-03-08 16:00:36 +0800

    added else case for numbers being the same

--- compare_two_numbers_print_bigger_number.py
+++ compare_two_numbers_print_bigger_number.py
@@ -3,5 +3,7 @@ second_number = float(input("Enter the second number: "))
 
 if first_number > second_number:
     print(f"The bigger number is {first_number}.")
-else:
+elif second_number > first_number:
     print(f"The bigger number is {second_number}.")
+else:
+    print("The numbers are the same and just equal.")
