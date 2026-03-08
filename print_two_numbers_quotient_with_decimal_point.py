aecb2dbbf7e661ad7d937e3cfc65fa460327f01a
Kryzle <kryzlesayat@gmail.com>
2026-03-08 18:05:06 +0800

    added equation and prints quotient with decimal point

--- print_two_numbers_quotient_with_decimal_point.py
+++ print_two_numbers_quotient_with_decimal_point.py
@@ -1,2 +1,3 @@
 first_number = float(input("Enter the first number: "))
 second_number = float(input("Enter the second number: "))
+print(f"The quotient of two numbers is ", first_number / second_number)
