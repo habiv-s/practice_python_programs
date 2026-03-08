0a1281178eea82e589ba632c351b4e80889984ce
Kryzle <kryzlesayat@gmail.com>
2026-03-08 21:43:23 +0800

    added equation and prints remainder

--- print_first_number_remainder.py
+++ print_first_number_remainder.py
@@ -1,2 +1,3 @@
 first_number = float(input("Enter the first number: "))
 second_number = float(input("Enter the second number: "))
+print(f"The remainder of the first number when divided by the second number is: ", first_number % second_number)
