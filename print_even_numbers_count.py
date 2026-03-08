d7b4f7b0ff0c59d64b96438e02bdb7c62f963d5f
Kryzle <kryzlesayat@gmail.com>
2026-03-08 19:52:41 +0800

    added modulus condition to detect even numbers and print count

--- print_even_numbers_count.py
+++ print_even_numbers_count.py
@@ -1,2 +1,6 @@
+check_even_numbers = 0
 for i in range(10):
     number = int(input("Enter number: "))
+    if number % 2 == 0:
+        check_even_numbers += 1
+print(f"EVEN NUMBERS COUNT: ", check_even_numbers)
