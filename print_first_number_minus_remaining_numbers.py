170e6cbcdc6b7f17e49470107dccf7b81cc4b355
Kryzle <kryzlesayat@gmail.com>
2026-03-08 22:20:22 +0800

    added separate result variable for cleaner subtraction calculation

--- print_first_number_minus_remaining_numbers.py
+++ print_first_number_minus_remaining_numbers.py
@@ -1,5 +1,6 @@
 first_number = float(input("Enter a number: "))
+result = first_number
 for i in range(9):
     numbers_to_minus = float(input("Enter a number: "))
-    first_number -= numbers_to_minus
-print(f"The result of the first number minus all of the remaining numbers is", first_number)
+    result -= numbers_to_minus
+print(f"The result of the first number minus all of the remaining numbers is", result)
