9a1e537872b995877fabaef8f4e262fe7e28cd49
Kryzle <kryzlesayat@gmail.com>
2026-03-08 23:03:04 +0800

    added end parameter for cleaner print format

--- print_all_numbers_between_two_numbers.py
+++ print_all_numbers_between_two_numbers.py
@@ -3,9 +3,9 @@ second_number = int(input("Enter the second number: "))
 
 if first_number < second_number:
     for i in range(first_number+1, second_number):
-        print(i)
+        print(i, end=" ")
 elif second_number < first_number:
     for i in range(second_number+1, first_number):
-        print(i)
+        print(i, end=" ")
 else:
     print("The numbers you entered are just the same, so it does not have numbers in between.")
