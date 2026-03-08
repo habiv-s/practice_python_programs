9dab0ae6d6bdc99aa9333fe7f4e8d84e09c1dbaa
Kryzle <kryzlesayat@gmail.com>
2026-03-08 21:12:47 +0800

    added condition to see if second number is less than first number

--- compare_two_numbers_print_smaller_number.py
+++ compare_two_numbers_print_smaller_number.py
@@ -3,5 +3,5 @@ second_number = float(input("Enter the second number: "))
 
 if first_number < second_number:
     print(f"The smaller number is {first_number}.")
-else:
+elif second_number < first_number:
     print(f"The smaller number is {second_number}.")
