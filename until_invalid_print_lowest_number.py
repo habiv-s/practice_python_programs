f04a059a211b9563f68964e57d8644917b904b4f
Kryzle <kryzlesayat@gmail.com>
2026-03-11 19:37:37 +0800

    added check for empty list before using min() to avoid errors

--- until_invalid_print_lowest_number.py
+++ until_invalid_print_lowest_number.py
@@ -5,5 +5,8 @@ while True:
         number = float(input("Enter number: "))
         numbers_list.append(number)
     except ValueError:
-        print(f"Invalid Input. The lowest number among all inputs is ", min(numbers_list))
+        if numbers_list:
+            print(f"Invalid Input. The lowest number among all inputs is ", min(numbers_list))
+        else:
+            print("Invalid Input. No valid numbers were entered.")
         break
