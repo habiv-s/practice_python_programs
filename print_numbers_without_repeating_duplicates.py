c25e385bccb65f4a1571f804af90c3abe605b79a
Kryzle <kryzlesayat@gmail.com>
2026-03-11 18:18:01 +0800

    added logic to display numbers without repeating duplicates, keeping first entry only

--- print_numbers_without_repeating_duplicates.py
+++ print_numbers_without_repeating_duplicates.py
@@ -2,6 +2,7 @@ numbers_list = [ ]
 
 for i in range(10):
     number = float(input("Enter number: "))
-    numbers_list.append(number)
+    if number not in numbers_list:
+        numbers_list.append(number)
 
 print(numbers_list)
