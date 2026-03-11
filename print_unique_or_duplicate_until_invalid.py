cc63bad2efd91d63707d803f91550cc951840208
Kryzle <kryzlesayat@gmail.com>
2026-03-11 18:56:46 +0800

    added list and logic to check if number is duplicate, prints Unique for new numbers

--- print_unique_or_duplicate_until_invalid.py
+++ print_unique_or_duplicate_until_invalid.py
@@ -1,6 +1,11 @@
+numbers_list = [ ]
+
 while True:
     try:
         number = float(input("Enter number: "))
+        if number not in numbers_list:
+            numbers_list.append(number)
+            print("Unique")
     except ValueError:
         print("Invalid Input")
         break
