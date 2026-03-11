258597161bb898745c1b357fb83d2facd467bac9
Kryzle <kryzlesayat@gmail.com>
2026-03-11 17:57:07 +0800

    added duplicate checking and store duplicate numbers in a list then prints them

--- print_duplicate_numbers.py
+++ print_duplicate_numbers.py
@@ -1,7 +1,12 @@
 numbers_list = [ ]
+duplicate_numbers_list = [ ]
 
 for i in range(10):
     number = float(input("Enter number: "))
     numbers_list.append(number)
 
-print(numbers_list)
+for number in numbers_list:
+    if numbers_list.count(number) != 1:
+        duplicate_numbers_list.append(number)
+
+print(duplicate_numbers_list)
