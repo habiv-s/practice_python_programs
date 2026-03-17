0f690339ac68a1035d65aa1524853c07078b8eb7
Kryzle <kryzlesayat@gmail.com>
2026-03-18 04:21:36 +0800

    added check for empty list before using max() to avoid errors

--- until_invalid_print_highest_number.py
+++ until_invalid_print_highest_number.py
@@ -5,5 +5,8 @@ while True:
         number = float(input("Enter number: "))
         numbers_list.append(number)
     except ValueError:
-        print(f"Invalid Input. The highest number among all inputs is ", max(numbers_list))
+        if numbers_list:
+            print(f"Invalid Input. The highest number among all inputs is ", max(numbers_list))
+        else:
+            print("Invalid Input. No valid numbers were entered.")
         break
