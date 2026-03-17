de1a3ccf9c91662f30793733c2193ae21cd73d20
Kryzle <kryzlesayat@gmail.com>
2026-03-18 04:12:54 +0800

    added reverse=True to sort list in descending order

--- until_invalid_print_highest_to_lowest_number.py
+++ until_invalid_print_highest_to_lowest_number.py
@@ -5,6 +5,6 @@ while True:
         number = float(input("Enter number: "))
         numbers_list.append(number)
     except ValueError:
-        numbers_list.sort()
-        print("Invalid Input", numbers_list)
+        numbers_list.sort(reverse=True)
+        print(f"Invalid Input. Here's the highest to lowest number inputs: ", numbers_list)
         break
