18089df8aa6993c9daf6119899edb00b7123a0f2
Kryzle <kryzlesayat@gmail.com>
2026-03-18 04:19:58 +0800

    changed print statement to include max() function in f-string message

--- until_invalid_print_highest_number.py
+++ until_invalid_print_highest_number.py
@@ -5,5 +5,5 @@ while True:
         number = float(input("Enter number: "))
         numbers_list.append(number)
     except ValueError:
-        print("Invalid Input")
+        print(f"Invalid Input. The highest number among all inputs is ", max(numbers_list))
         break
