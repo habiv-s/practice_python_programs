06e578619b67363e9c8704af3c9f867fcce83dc9
Kryzle <kryzlesayat@gmail.com>
2026-03-18 06:00:57 +0800

    added check for empty list

--- until_invalid_print_average.py
+++ until_invalid_print_average.py
@@ -5,6 +5,9 @@ while True:
         number = float(input("Enter number: "))
         numbers_list.append(number)
     except ValueError:
-        average = sum(numbers_list) / len(numbers_list)
-        print(f"Invalid Input. The average is: {average}")
+        if numbers_list:
+            average = sum(numbers_list) / len(numbers_list)
+            print(f"Invalid Input. The average is: {average}")
+        else:
+            print("Invalid Input. No valid numbers were entered.")
         break
