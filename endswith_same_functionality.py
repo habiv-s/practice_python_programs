a9c5d61fb1f9d9bf297c58046006e56c45d49693
Kryzle <kryzlesayat@gmail.com>
2026-03-18 20:54:23 +0800

    added else if string does not end with ending input

--- endswith_same_functionality.py
+++ endswith_same_functionality.py
@@ -3,3 +3,5 @@ ending = input("Enter the ending: ")
 
 if text[-len(ending):] == ending:
     print(f"The string ends with '{ending}'.")
+else:
+    print(f"The string does NOT end with '{ending}'.")
