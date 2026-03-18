99fa5468fcd1a8ebaa4e9d40a26959f7dfbf238a
Kryzle <kryzlesayat@gmail.com>
2026-03-18 21:12:31 +0800

    added condition to remove suffix only when text length is sufficient to match suffix, this is to avoid slicing errors

--- removesuffix_same_functionality.py
+++ removesuffix_same_functionality.py
@@ -1,7 +1,7 @@
 text = input("Enter a string: ")
 suffix = input("Enter the suffix to remove: ")
 
-if text[-len(suffix):] == suffix:
+if len(text) >= len(suffix) and text[-len(suffix):] == suffix:
     print(text[:-len(suffix)])
 else:
     print(text)
