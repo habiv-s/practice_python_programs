cecf792247ae21b735b1f1be7413a0199e16a3a3
Kryzle <kryzlesayat@gmail.com>
2026-03-18 18:10:16 +0800

    updated while loop to skip all leading whitespace characters including spaces, tabs, and newlines before printing trimmed input

--- lstrip_same_functionality.py
+++ lstrip_same_functionality.py
@@ -1,5 +1,5 @@
 user_input = input("Enter anything: ")
 index = 0
-while index < len(user_input) and user_input[index] == " ":
+while index < len(user_input) and user_input[index] in (" ", "\t", "\n"):
     index += 1
 print(user_input[index:])
