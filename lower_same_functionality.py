c85760e472f0fc24f06d84be3cfc9e10a4164c4a
Kryzle <kryzlesayat@gmail.com>
2026-03-18 19:27:16 +0800

    added check for capital letters and manually converted input to lowercase using ASCII (adding 32)

--- lower_same_functionality.py
+++ lower_same_functionality.py
@@ -1,2 +1,10 @@
 text = input("Enter a string in an incorrect casing: ")
-print(text)
+lowercase_text = ""
+
+for character in text:
+    if 'A' <= character <= 'Z':
+        lowercase_text += chr(ord(character) + 32)
+    else:
+        lowercase_text += character
+
+print(lowercase_text)
