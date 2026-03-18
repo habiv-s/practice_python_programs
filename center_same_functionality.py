412fca80421c9b4f94ea30656c0469162dc0f4de
Kryzle <kryzlesayat@gmail.com>
2026-03-18 23:20:54 +0800

    added left and right space calculation to center the string output based on total width then prints new output

--- center_same_functionality.py
+++ center_same_functionality.py
@@ -2,5 +2,7 @@ text = input("Enter a string: ")
 width = int(input("Enter the total width: "))
 
 spaces_to_add = max(0, width - len(text))
+left_space = spaces_to_add // 2
+right_space = spaces_to_add - left_space
 
-print(text)
+print(f"'", " " * left_space + text + " " * right_space, "'")
