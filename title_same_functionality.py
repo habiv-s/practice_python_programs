f53651b403aa08fec0b84362c433f09b5a35d412
Kryzle <kryzlesayat@gmail.com>
2026-03-19 23:36:44 +0800

    added loop to iterate through input string and build result variable, also added logic to detect spaces and identify new words

--- title_same_functionality.py
+++ title_same_functionality.py
@@ -1,3 +1,10 @@
 text = input("Enter a string in an incorrect casing: ")
+result = ""
+new_word = True
+
+for character in text:
+    if character == " ":
+        result += " "
+        new_word = True
 
 print(text)
