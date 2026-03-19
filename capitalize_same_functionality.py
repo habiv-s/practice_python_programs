96196f3a5db32977f8bdeb7855a3bc95507c305d
Kryzle <kryzlesayat@gmail.com>
2026-03-19 23:58:46 +0800

    added logic to convert remaining uppercase characters to lowercase and also else that keeps character unchanged

--- capitalize_same_functionality.py
+++ capitalize_same_functionality.py
@@ -4,5 +4,9 @@ result = ""
 for index in range(len(text)):
     if index == 0 and 'a' <= text[index] <= 'z':
         result += chr(ord(text[index]) -32 )
-
+    elif index > 0 and 'A' <= text[index] <= 'Z':
+        result += chr(ord(text[index]) + 32)
+    else:
+        result += text[index]
+        
 print(f"Result: {result}")
