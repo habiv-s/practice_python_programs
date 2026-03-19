b445537e6efff46860a2f78dfe8e1a911d5d6925
Kryzle <kryzlesayat@gmail.com>
2026-03-19 23:16:00 +0800

    implemented swapcase functionality by reversing the case of each character without using built-in function

--- swapcase_same_functionality.py
+++ swapcase_same_functionality.py
@@ -1,3 +1,12 @@
 text = input("Enter a string in an incorrect casing: ")
+swap_casing_text = ""
 
-print(text)
+for character in text:
+    if 'A' <= character <= 'Z':
+        swap_casing_text += chr(ord(character) + 32)
+    elif 'a' <= character <= 'z':
+        swap_casing_text += chr(ord(character) - 32)
+    else:
+        swap_casing_text += character
+
+print(swap_casing_text)
