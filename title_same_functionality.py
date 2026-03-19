33bbf06bf900fcb1580e8860de4279ccd75f5321
Kryzle <kryzlesayat@gmail.com>
2026-03-19 23:38:32 +0800

    implemented capitalization of first letter of each word using ASCII conversion

--- title_same_functionality.py
+++ title_same_functionality.py
@@ -6,5 +6,11 @@ for character in text:
     if character == " ":
         result += " "
         new_word = True
-
+    elif new_word:
+        if 'a' <= character <= 'z':
+            result += chr(ord(character) - 32)
+        else:
+            result += character
+        new_word = False
+        
 print(text)
