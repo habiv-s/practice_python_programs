40ce8a804b620e39a383dde668d59cfa61851ab6
Kryzle <kryzlesayat@gmail.com>
2026-03-20 00:56:50 +0800

    updated while loop to skip all trailing whitespace characters including spaces, tabs, and newlines before printing trimmed input

--- rstrip_same_functionality.py
+++ rstrip_same_functionality.py
@@ -1,5 +1,5 @@
 user_input = input("Enter anything: ")
 index = len(user_input) - 1
-while index >= 0  and user_input[index] == " ":
+while index >= 0  and user_input[index] in (" ", "\t", "\n"):
     index -= 1
 print("'" + user_input[:index + 1] + "'")
