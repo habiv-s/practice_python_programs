aacf0b7dbd2cf2ba0d30a7839da86250a5f56fe4
Kryzle <kryzlesayat@gmail.com>
2026-03-19 23:56:00 +0800

    added loop using index to iterate through input string and logic to capitalize first character using ASCII conversion

--- capitalize_same_functionality.py
+++ capitalize_same_functionality.py
@@ -1,4 +1,8 @@
 text = input("Enter a string in an incorrect casing: ")
 result = ""
 
+for index in range(len(text)):
+    if index == 0 and 'a' <= text[index] <= 'z':
+        result += chr(ord(text[index]) -32 )
+
 print(text)
