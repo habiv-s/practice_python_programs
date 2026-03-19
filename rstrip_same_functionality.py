373f47316d5ac9c3754451b6a452a9c92db66703
Kryzle <kryzlesayat@gmail.com>
2026-03-20 00:55:12 +0800

    updated print for better output presentation

--- rstrip_same_functionality.py
+++ rstrip_same_functionality.py
@@ -2,4 +2,4 @@ user_input = input("Enter anything: ")
 index = len(user_input) - 1
 while index >= 0  and user_input[index] == " ":
     index -= 1
-print(user_input[:index + 1])
+print("'" + user_input[:index + 1] + "'")
