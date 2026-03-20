41f53f0aaace330368677a17d1b546f5ee7a29f4
Kryzle <kryzlesayat@gmail.com>
2026-03-20 14:37:34 +0800

    added check for lowercase letters and manually converted input to uppercase using ASCII (subtracting 32)

--- upper_same_functionality.py
+++ upper_same_functionality.py
@@ -1,2 +1,10 @@
 text = input("Enter a string in an incorrect casing: ")
+uppercase_text = ""
+
+for character in text:
+    if 'a' <= character <= 'z':
+        uppercase_text += chr(ord(character) - 32)
+    else:
+        uppercase_text += character
+        
 print(text)
