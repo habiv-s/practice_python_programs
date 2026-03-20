b796e26e72d5d77bd619aa2ab9f57ace9c0bb5ff
Kryzle <kryzlesayat@gmail.com>
2026-03-20 15:39:41 +0800

    added else if string does not start with beginning input

--- startswith_same_functionality.py
+++ startswith_same_functionality.py
@@ -3,3 +3,5 @@ beginning = input("Enter the beginning: ")
 
 if text[:len(beginning)] == beginning:
     print(f"The string starts with '{beginning}'.")
+else:
+    print(f"The string does NOT start with '{beginning}'.")
