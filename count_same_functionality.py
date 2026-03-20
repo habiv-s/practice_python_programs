05264450884aefa75dfbbe75e764758c7d7b1cde
Kryzle <kryzlesayat@gmail.com>
2026-03-20 17:03:00 +0800

    removed substring_length variable

--- count_same_functionality.py
+++ count_same_functionality.py
@@ -1,11 +1,9 @@
 text = input("Enter main string: ")
 substring = input("Enter substring to count: ")
-
 count = 0
-substring_length = len(substring)
 
-for i in range(len(text) - substring_length + 1):
-    if text[i:i + substring_length] == substring:
+for i in range(len(text) - len(substring) + 1):
+    if text[i:i + len(substring)] == substring:
         count += 1
 
 print(count)
