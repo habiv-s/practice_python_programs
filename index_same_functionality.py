78e4644ccc5f99a93e1496071a68968596e5ead9
Kryzle <kryzlesayat@gmail.com>
2026-03-20 17:42:20 +0800

    added check for first occurrence of substring in main string using slicing and loop, then prints index

--- index_same_functionality.py
+++ index_same_functionality.py
@@ -1,2 +1,7 @@
 text = input("Enter main string: ")
 substring = input("Enter substring to find: ")
+
+for i in range(len(text) - len(substring) + 1):
+    if text[i:i + len(substring)] == substring:
+        print(i)
+        break
