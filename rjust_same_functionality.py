6133d2601d573cb496ba595991894b10cc41e83a
Kryzle <kryzlesayat@gmail.com>
2026-03-20 15:47:36 +0800

    used max() to simplify padding calculation for rjust and prevent negative values then prints result

--- rjust_same_functionality.py
+++ rjust_same_functionality.py
@@ -1,4 +1,7 @@
 text = input("Enter a string: ")
 width = int(input("Enter the total width: "))
 
-print(text)
+spaces_to_add = max(0, width - len(text))
+result = (" " * spaces_to_add) + text
+
+print(f"Result: '{result}'")
